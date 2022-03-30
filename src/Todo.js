21c6ba084e08bda5903f192ef9bfcacc5d9ef1a9
xuanhieu <xhieu3@gmail.com>
2022-03-30 19:18:01 +0700

    test

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+
+const todo = () => {
+
+    
+    return (
+        <>
+            <h2 className='button'>change button</h2>
+        </>
+    );
+}
+
+export default todo; 
